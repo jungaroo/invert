1e90bb4f39d0d23dec4c631cc80f51b7dfaa3cd9
odoland <orlando.wong@yahoo.com>
2016-07-19 16:30:52 -0700

    Create doubler.c

---
+++ doubler.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+// This program will double your number!
+
+int main()
+{
+  
+  int number;
+  int doublenumber;
+  
+  printf("Hello, please input a number!\n");
+  scanf("%d,&number")
+  
+  doublenumber = number * 2
+  
+  printf("Ok, I will now double your number. Please wait..");
+  
+  printf("You input $number and the result is $doublenumber");
+  
+  return 0;
+
+}
